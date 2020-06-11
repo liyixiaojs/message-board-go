7a39c736a505feb91fc82dbabf92b972c4fbb3fe
liyixiao <liyixiaojs@gmail.com>
2020-06-11 21:39:36 +0800

    feat: update travis

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+rm -rf ./main;
+GOARCH=amd64 GOOS=linux go build -o main *.go;
+scp ./main root@182.92.79.110:~/golang/;
+scp ./conf.yml root@182.92.79.110:~/golang/;
+CP="cd ~/golang;";
+ssh root@182.92.79.110 "${CP}";
