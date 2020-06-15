17963b1c1a6f7c488b0cb7662fc979e0ca9032c8
liyixiao <liyixiaojs@gmail.com>
2020-06-15 16:24:17 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
 # rm -rf ./main;
 # GOARCH=amd64 GOOS=linux go build -o main *.go;
+echo 'Starting to COPY'
+
 scp ./main root@182.92.79.110:~/golang/;
-scp ./conf.yml root@182.92.79.110:~/golang/;
+scp ./conf.yaml root@182.92.79.110:~/golang/;
 CP="cd ~/golang;";
 ssh root@182.92.79.110 "${CP}";
