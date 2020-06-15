c49af0f9a8e66f7828a30136d0713484e4ce17b7
liyixiao <liyixiaojs@gmail.com>
2020-06-15 16:41:36 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # rm -rf ./main;
 # GOARCH=amd64 GOOS=linux go build -o main *.go;
+ssh-keyscan -H 182.92.79.110 >> ~/.ssh/known_hosts
 echo 'Step 1'
-scp ./main root@182.92.79.110:~/golang/;
+scp -o StrictHostKeyChecking=no ./main root@182.92.79.110:~/golang/
 echo 'Step 2'
-scp ./conf.yaml root@182.92.79.110:~/golang/;
+scp -o StrictHostKeyChecking=no ./conf.yaml root@182.92.79.110:~/golang/
 
 echo 'Step 3'
-CP="cd ~/golang;";
-ssh root@182.92.79.110 "${CP}";
+CP="cd ~/golang;"
+ssh root@182.92.79.110 "${CP}"
 
 echo 'Done'
