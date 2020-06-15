582facaa502f6a4706f63e9e9b19249dcd05abb2
liyixiao <liyixiaojs@gmail.com>
2020-06-15 16:33:22 +0800

    update .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 # rm -rf ./main;
 # GOARCH=amd64 GOOS=linux go build -o main *.go;
-echo 'Starting to COPY'
-
+echo 'Step 1'
 scp ./main root@182.92.79.110:~/golang/;
+echo 'Step 2'
 scp ./conf.yaml root@182.92.79.110:~/golang/;
+
+echo 'Step 3'
 CP="cd ~/golang;";
 ssh root@182.92.79.110 "${CP}";
+
+echo 'Done'
