552a2f3fb6fd5ae3048f7ea04865164db177dd8c
liyixiao <liyixiaojs@gmail.com>
2020-06-15 16:52:27 +0800

    update .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 # rm -rf ./main;
 # GOARCH=amd64 GOOS=linux go build -o main *.go;
-ssh-keyscan -H 182.92.79.110 >> ~/.ssh/known_hosts
-echo 'Step 1'
-scp -o StrictHostKeyChecking=no ./main root@182.92.79.110:~/golang/
-echo 'Step 2'
-scp -o StrictHostKeyChecking=no ./conf.yaml root@182.92.79.110:~/golang/
 
-echo 'Step 3'
-CP="cd ~/golang;"
+echo 'Step 1'
+CP="ls ~/golang;"
 ssh root@182.92.79.110 "${CP}"
 
 echo 'Done'
